05597e34ec062851360cb6694ab86b803c50465c
Abedela Bedru <69614693+codistiano@users.noreply.github.com>
2024-11-06 10:42:33 +0300

    Exercise 2.1 Submission

---
+++ Part-2/courseinfo/src/App.jsx
@@ -0,0 +1,60 @@
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  );
+};
+
+const Header = ({ name }) => {
+  return (
+    <h1>{name}</h1>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      <Parts part={parts[0]} />
+      <Parts part={parts[1]} />
+      <Parts part={parts[2]} />
+    </div>
+  )
+}
+
+const Parts = ({part}) => {
+  return (
+    <>
+      <p>{part.name} {part.exercises}</p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+export default App;
