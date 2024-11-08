8d2daff9eda27521e3fe318eca5115b4275e36c0
Abedela Bedru <69614693+codistiano@users.noreply.github.com>
2024-11-08 03:15:44 +0300

    Ex 2.10 Submission

--- Part-2/Phonebook/src/App.jsx
+++ Part-2/Phonebook/src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
-
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNum, setNewNum] = useState("");
   const [search, setSearch] = useState("");
@@ -41,22 +36,36 @@ const App = () => {
   const filtering = (e) => {
     const searchedKey = e.target.value;
     setSearch(searchedKey);
-  
-      const filtered = persons.filter((person) => {
-        return person.name.toLowerCase().includes(searchedKey.toLowerCase());
-      });
 
-      setResult(filtered);
+    const filtered = persons.filter((person) => {
+      return person.name.toLowerCase().includes(searchedKey.toLowerCase());
+    });
+
+    setResult(filtered);
   };
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((res) => {
+      setPersons(res.data);
+      setResult(res.data);
+    });
+  }, []);
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter filtering={filtering} search={search}  />
+      <Filter filtering={filtering} search={search} />
       <h3>Add a new</h3>
-      <PersonForm handleSubmit={handleSubmit} newName={newName} newNum={newNum} setNewName={setNewName} setNewNum={setNewNum}  />
+      <PersonForm
+        handleSubmit={handleSubmit}
+        newName={newName}
+        newNum={newNum}
+        setNewName={setNewName}
+        setNewNum={setNewNum}
+      />
       <h2>Numbers</h2>
-      <Persons result={result}  />
+      <Persons result={result} />
     </div>
   );
 };
