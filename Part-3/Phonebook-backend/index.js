dfbeb0168fc4a3009e6a5ac93db971e2d607e75c
Abedela Bedru <69614693+codistiano@users.noreply.github.com>
2024-11-11 09:41:42 +0300

    Exercise 3.1 - 3.6 Submission

---
+++ Part-3/Phonebook-backend/index.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/api/persons", (req, res) => {
+  res.send(persons);
+});
+
+app.get("/info", (req, res) => {
+  const usersAmount = persons.length;
+  const serverTime = new Date();
+  res.send(
+    "<p>Phonebook has info for " +
+      usersAmount +
+      " people</p><p>" +
+      serverTime +
+      "</p>"
+  );
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const contact = persons.find((person) => person.id === id);
+  if (contact) {
+    res.send(contact);
+  } else {
+    res.status(404).send("Contact not found");
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const index = persons.findIndex((person) => person.id === id);
+  if (index === -1) {
+    res.status(404).send("Contact not found");
+  } else {
+    persons.splice(index, 1);
+    res.send(persons);
+  }
+});
+
+app.post("/api/persons", (req, res) => {
+  const { name, number } = req.body;
+  const rndId = Math.random() * 94838493;
+  const newContact = { id: rndId.toString(), name, number };
+  const nameExists = persons.some((contact) => contact.name === name);
+  if (!name || !number) {
+    res.status(400).send({ error: "Name and number are required" });
+    return;
+  } else if (nameExists) {
+    res.status(400).send({ error: "name must be unique" });
+    return;
+  }
+  persons.push(newContact);
+  res.send(newContact);
+});
+
+
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
