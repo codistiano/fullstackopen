5ca88ad1fe72505abdd257e97a2fa056adc6e8a6
Abedela Bedru <69614693+codistiano@users.noreply.github.com>
2024-11-18 04:01:23 +0300

    Exercise 4.2 Submission
    - Separated app.js from index.js
    - Create Route handling directory

---
+++ Part-4/app.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(config.LOCAL_MONGODB_URI)
+.then(() => {
+    logger.info("Connected to the Database")
+})
+.catch((err) => {
+    logger.error("Could not connect to the Database", err)
+})
+
+app.use(express.json())
+app.use(cors())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
--- Part-4/index.js
+++ Part-4/index.js
@@ -1,43 +1,6 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app')
 const config = require('./utils/config')
-const Blog = require('./models/blog')
 const logger = require('./utils/logger')
-
-mongoose.set('strictQuery', false)
-
-const mongoUrl = config.LOCAL_MONGODB_URI
-
-mongoose.connect(mongoUrl)
-.then(() => {
-  logger.info('Connected to MongoDB')
-})
-.catch(error => {
-  logger.error('Error connecting to MongoDB:', error.message)
-})
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
