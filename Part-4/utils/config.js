45f8348ec0dafd36ef247582abaa3a7286fea54c
Abedela Bedru <69614693+codistiano@users.noreply.github.com>
2024-11-22 02:56:30 +0300

    Exercise 4.15 Submission

--- Part-4/utils/config.js
+++ Part-4/utils/config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const config = {
   PORT: process.env.PORT || 3001,
-  MONGODB_URI: process.env.NODE_ENV==='test'? process.env.LOCAL_MONGODB_URI : process.env.MONGODB_URI,
+  MONGODB_URI: process.env.NODE_ENV==='test' || process.env.NODE_ENV==='development'? process.env.LOCAL_MONGODB_URI : process.env.MONGODB_URI,
 }
 
 module.exports = config
