f0326c0b011197449474b7f60a535802ae79b108
Abedela Bedru <69614693+codistiano@users.noreply.github.com>
2024-11-28 17:00:31 +0300

    Exercise 5.7 Submission

--- Part-5/bloglist-frontend/src/components/Blog.jsx
+++ Part-5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,28 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} - {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+  
+const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} - {blog.author} <button onClick={() => setVisible((prev) => !prev)}>{ visible ? 'hide' : 'view'}</button>
+      <div>
+        <div style={{ display: visible ? '' : 'none'}}>
+          <p>{blog.url}</p>
+          <p>likes {blog.likes} <button>Like</button></p>
+          <p>{blog.user.name}</p>
+        </div>
+      </div>
+    </div>  
+  )
+}
 
 export default Blog
