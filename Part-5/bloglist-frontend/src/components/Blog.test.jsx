c748121da09449b7891760ec25f477fe71119830
Abedela Bedru <69614693+codistiano@users.noreply.github.com>
2024-12-01 16:49:13 +0300

    Exercise 5.14 Submission

--- Part-5/bloglist-frontend/src/components/Blog.test.jsx
+++ Part-5/bloglist-frontend/src/components/Blog.test.jsx
@@ -36,3 +36,40 @@ test("renders the blogs title and author but not the url or like no.s by default
   expect(title).toBeDefined();
   expect(hiddenDetails).toHaveStyle({ display: "none" })
 });
+
+test("the url and no of likes are shown when the buttong controlling the shown details has been clicked", () => {
+    const blog = {
+        title: "Testing sucks! Here's how to make it easy.",
+        author: "Abudi",
+        url: "https://codistiano.com/blogs/Testing_sucks_heres_how_to_make_it_easy",
+        likes: 0,
+        id: "6748729258a21b5de996c9a4",
+        user: {
+            username: "Abudi",
+            name: "Abudi",
+            blogs: [],
+            id: "6746fa846a982d91581c4fbf",
+        },
+    };
+
+    const user = {
+        username: "Abudi",
+        name: "Abudi",
+        blogs: [],
+        id: "6746fa846a982d91581c4fbf",
+    };
+
+    render(<Blog blog={blog} user={user} />);
+
+    const showDetailsButton = screen.getByText("view");
+    showDetailsButton.click();
+
+    const url = screen.getByText(
+        "https://codistiano.com/blogs/Testing_sucks_heres_how_to_make_it_easy",
+        { exact: false }
+    );
+    const likes = screen.getByText("likes 0");
+
+    expect(url).toBeDefined();
+    expect(likes).toBeDefined();
+})
